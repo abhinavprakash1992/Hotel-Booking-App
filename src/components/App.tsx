4318a495ef0bee7b3b25ed432f174b7e675ac66f
Abhinav prakash <abhinav.prakash1992@gmail.com>
2021-06-19 01:08:12 +0530

    fix(app.tsx): remove test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -179,7 +179,7 @@ export default class App extends React.Component<{}, State> {
             style={{ marginRight: "10px" }}
             size={"lg"}
           />
-          Choose number of peoples abc
+          Choose number of peoples
         </div>
         <table >
           <thead>
